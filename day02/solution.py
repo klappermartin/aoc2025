3966bbca8eb9be061da6681b276ddad3bca18c63
Martin Klapper <64156820+klapperking@users.noreply.github.com>
2025-12-02 23:33:34 +0100

    share code where possible and better control flow

--- day02/solution.py
+++ day02/solution.py
@@ -1,21 +1,17 @@
 import argparse
 import sys
-from typing import Literal
+from typing import Callable
 
 
-def is_invalid_id(id: int):
-    id_str = str(id)
-    middle = len(id_str) // 2
-
-    if len(id_str) % 2 == 0 and id_str[:middle] == id_str[middle:]:
-        return True
-
-    return False
+def is_invalid_id(num: int) -> bool:
+    num_str = str(num)
+    middle = len(num_str) // 2
+    return len(num_str) % 2 == 0 and num_str[:middle] == num_str[middle:]
 
 
-def is_invalid_id_p2(id: int):
-    id_str = str(id)
-    length = len(id_str)
+def is_invalid_id_p2(num: int) -> bool:
+    num_str = str(num)
+    length = len(num_str)
 
     # check all sample_sizes for repeating
     for sample_size in range(1, length // 2 + 1):
@@ -23,42 +19,44 @@ def is_invalid_id_p2(id: int):
         if length % sample_size != 0:
             continue
 
-        repeated_sample = id_str[:sample_size] * (length // sample_size)
+        # repeat sample as often as it fits, then compare with initial id
+        repeated_sample = num_str[:sample_size] * (length // sample_size)
 
-        if repeated_sample == id_str:
+        if repeated_sample == num_str:
             return True
 
     return False
 
 
-def main(part: Literal[1] | Literal[2]):
+def parse_input(input_data: str) -> list[tuple[int, int]]:
+    ranges = []
+    for range_str in input_data.split(","):
+        start, end = [int(x) for x in range_str.split("-")]
+        ranges.append((start, end))
+    return ranges
+
+
+def calculate_solution(
+    ranges: list[tuple[int, int]], check_valid_id: Callable[[int], bool]
+) -> int:
     solution = 0
+    for start, end in ranges:
+        for num in range(start, end + 1):
+            if check_valid_id(num):
+                solution += num
+    return solution
 
-    input_file = sys.stdin.read()
-    input = input_file.split(",")
 
-    if part == 1:
-        for range in input:
-            start, end = [int(x) for x in range.split("-")]
+def part1(input_data: str) -> None:
+    ranges = parse_input(input_data)
+    result = calculate_solution(ranges, is_invalid_id)
+    print(result)
 
-            while start < end + 1:
-                if is_invalid_id(start):
-                    solution += start
 
-                start += 1
-
-    else:
-        # we could do significantly fewer checks
-        for range in input:
-            start, end = [int(x) for x in range.split("-")]
-
-            while start < end + 1:
-                if is_invalid_id_p2(start):
-                    solution += start
-
-                start += 1
-
-    print(solution)
+def part2(input_data: str) -> None:
+    ranges = parse_input(input_data)
+    result = calculate_solution(ranges, is_invalid_id_p2)
+    print(result)
 
 
 if __name__ == "__main__":
@@ -67,4 +65,9 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    main(args.part)
+    input_data = sys.stdin.read()
+
+    if args.part == 1:
+        part1(input_data)
+    else:
+        part2(input_data)
